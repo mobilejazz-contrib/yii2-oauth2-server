681fc5c954030a32a37fb67a46232e1f34d5ec01
Brian McGee <brian@mobilejazz.com>
2017-04-25 07:53:51 +0200

    Added active record user model

---
+++ models/OauthUsers.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace mobilejazz\yii2\oauth2server\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "oauth_clients".
+ *
+ * @property string $client_id
+ * @property string $client_secret
+ * @property string $redirect_uri
+ * @property string $grant_types
+ * @property string $scope
+ * @property integer $user_id
+ *
+ * @property OauthAccessTokens[] $oauthAccessTokens
+ * @property OauthAuthorizationCodes[] $oauthAuthorizationCodes
+ * @property OauthRefreshTokens[] $oauthRefreshTokens
+ */
+class OauthUsers extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%oauth_users}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['username'], 'required'],
+            [['username', 'first_name', 'last_name'], 'string', 'max' => 255],
+            [['password'], 'string', 'max' => 2000]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'username' => 'Username',
+            'first_name' => 'First Name',
+            'last_name' => 'Last Name',
+            'password' => 'Password'
+        ];
+    }
+
+}
